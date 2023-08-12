56c91c0b5a86c56afb96f5d332a0f522e6dba0ab
DarkTinia <1106116020@qq.com>
2023-08-12 06:34:35 -0700

    Finish Assignment1

--- Assignment1-Introduction_to_LLVM/LocalOpts/lib/3-MultiInstOpt.cpp
+++ Assignment1-Introduction_to_LLVM/LocalOpts/lib/3-MultiInstOpt.cpp
@@ -1,106 +1,117 @@
 #include "LocalOpts.h"
+#include <cstddef>
 #include <llvm-16/llvm/ADT/APInt.h>
 #include <llvm-16/llvm/IR/BasicBlock.h>
+#include <llvm-16/llvm/IR/Constant.h>
 #include <llvm-16/llvm/IR/Constants.h>
 #include <llvm-16/llvm/IR/IRBuilder.h>
 #include <llvm-16/llvm/IR/InstrTypes.h>
 #include <llvm-16/llvm/IR/Instruction.h>
 #include <llvm-16/llvm/IR/Value.h>
+#include <vector>
 
 using namespace llvm;
 
-bool isOpVar(BinaryOperator *BO, Value *Var, ConstantInt *Con, bool &IsLeft) {
-  bool Flag = false;
-  Value *Operand0 = BO->getOperand(0);
-  Value *Operand1 = BO->getOperand(1);
+bool insCon(const Instruction &I) {
+  return isa<Instruction>(I.getOperand(0)) && isa<ConstantInt>(I.getOperand(1));
+}
 
-  if (ConstantInt *C = dyn_cast<ConstantInt>(Operand0)) {
-    if (ConstantInt *C1 = dyn_cast<ConstantInt>(Operand1)) {
-      Flag = false;
-    } else {
-      Var = Operand1;
-      Con = C;
-      IsLeft = true;
-      Flag = true;
-    }
-  } else if (ConstantInt *C = dyn_cast<ConstantInt>(Operand1)) {
-    Var = Operand0;
-    Con = C;
-    IsLeft = false;
-    Flag = true;
-  }
-
-  return Flag;
+bool conIns(const Instruction &I) {
+  return isa<ConstantInt>(I.getOperand(0)) && isa<Instruction>(I.getOperand(1));
 }
 
 PreservedAnalyses MultiInstOptPass::run([[maybe_unused]] Function &F,
                                         FunctionAnalysisManager &) {
 
-  /// @todo(CSCD70) Please complete this method.
+  // @todo(CSCD70) Please complete this method.
   int Modify = 0;
+  std::vector<Instruction *> DeleteInst;
+
   for (auto BB = F.begin(); BB != F.end(); BB++) {
     for (auto Ins = BB->begin(); Ins != BB->end(); Ins++) {
-      Instruction &Inscode = *Ins;
-      if (BinaryOperator *BO = dyn_cast<BinaryOperator>(&Inscode)) {
-        auto OP = BO->getOpcode();
-        Value *Operand0 = BO->getOperand(0);
-        Value *Operand1 = BO->getOperand(1);
+      Instruction &CurInscode = *Ins;
+      outs() << "current instruction is: " << CurInscode << "\n";
+      if (CurInscode.isBinaryOp()) {
+        Value *Operand0 = CurInscode.getOperand(0);
+        Value *Operand1 = CurInscode.getOperand(1);
 
-        ConstantInt *Con0;   // Constant for child
-        ConstantInt *Con1;   // Constant for parent
-        Value *Var;          // Variable
-        bool IsLeft = false; // just for Sub and Div
-
-        switch (OP) {
-
-        case Instruction::Add: {
-          outs() << "into add\n";
-          if (isOpVar(BO, Var, Con0, IsLeft)) {
-            outs() << "into 1\n";
-            if (BinaryOperator *PreBO = dyn_cast<BinaryOperator>(Var)) {
-              outs() << "into 2\n";
-              auto PreOP = PreBO->getOpcode();
-              if (PreOP == BinaryOperator::Add) {
-                outs() << "into 3\n";
-                if (isOpVar(PreBO, Var, Con1, IsLeft)) {
-                  outs() << "into 4\n";
-                  BO->setOperand(0, Var);
-                  int Newnum = Con0->getValue().getLimitedValue() +
-                               Con1->getValue().getLimitedValue();
-                  BO->setOperand(
-                      1, ConstantInt::getSigned(Con1->getType(), Newnum));
-                }
-              } else if (PreOP == BinaryOperator::Sub) {
-              }
-            }
+        Instruction *CurIns = nullptr;
+        ConstantInt *CurCon = nullptr;
+        int CurInsCon = -1;
+        if (insCon(CurInscode)) {
+          CurInsCon = 1;
+          CurIns = dyn_cast<Instruction>(Operand0);
+          CurCon = dyn_cast<ConstantInt>(Operand1);
+        } else if (conIns(CurInscode)) {
+          CurInsCon = 0;
+          CurIns = dyn_cast<Instruction>(Operand1);
+          CurCon = dyn_cast<ConstantInt>(Operand0);
+        } else {
+          continue;
+        }
+        int PreInsCon = -1;
+        ConstantInt *PreCon = nullptr;
+        Instruction *PreIns = nullptr;
+        if (CurIns->isBinaryOp()) {
+          if (insCon(*CurIns)) {
+            PreInsCon = 1;
+            PreIns = dyn_cast<Instruction>(CurIns->getOperand(0));
+            PreCon = dyn_cast<ConstantInt>(CurIns->getOperand(1));
+          } else if (conIns(*CurIns)) {
+            PreInsCon = 0;
+            PreIns = dyn_cast<Instruction>(CurIns->getOperand(1));
+            PreCon = dyn_cast<ConstantInt>(CurIns->getOperand(0));
+          } else {
+            continue;
           }
         }
-        case Instruction::FAdd:
-        case Instruction::Sub:
-        case Instruction::FSub:
-        case Instruction::Mul:
-        case Instruction::FMul:
-        case Instruction::UDiv:
-        case Instruction::SDiv:
-        case Instruction::FDiv:
-        case Instruction::URem:
-        case Instruction::SRem:
-        case Instruction::FRem:
-        case Instruction::Shl:
-        case Instruction::LShr:
-        case Instruction::AShr:
-        case Instruction::And:
-        case Instruction::Or:
-        case Instruction::Xor:
-        case Instruction::BinaryOpsEnd:
+        switch (CurInscode.getOpcode()) {
+        case Instruction::Add: {
+          if (CurIns->getOpcode() == Instruction::Sub) {
+            if (PreInsCon &&
+                (PreCon->getSExtValue() == CurCon->getSExtValue())) {
+              CurInscode.replaceAllUsesWith(PreIns);
+              DeleteInst.push_back(&CurInscode);
+            }
+          }
           break;
         }
+        case Instruction::Sub: {
+          if (CurIns->getOpcode() == Instruction::Add) {
+            if (CurInsCon &&
+                (PreCon->getSExtValue() == CurCon->getSExtValue())) {
+              CurInscode.replaceAllUsesWith(PreIns);
+              DeleteInst.push_back(&CurInscode);
+            }
+          }
+          break;
+        }
+        case Instruction::Mul: {
+          if (CurIns->getOpcode() == Instruction::SDiv) {
+            if (CurInsCon &&
+                (PreCon->getSExtValue() == CurCon->getSExtValue())) {
+              CurInscode.replaceAllUsesWith(PreIns);
+              DeleteInst.push_back(&CurInscode);
+            }
+          }
+          break;
+        }
+        case Instruction::SDiv: {
+          if (CurIns->getOpcode() == Instruction::Mul) {
+            if (PreCon->getSExtValue() == CurCon->getSExtValue()) {
+              CurInscode.replaceAllUsesWith(PreIns);
+              DeleteInst.push_back(&CurInscode);
+            }
+          }
+          break;
+        }
+        }
       }
-      if (Modify == 0) {
-        Ins++;
-      }
-      Modify = 0;
     }
+  }
+
+  for (auto &I : DeleteInst) {
+    I->eraseFromParent();
   }
 
   return PreservedAnalyses::none();
--- Assignment1-Introduction_to_LLVM/LocalOpts/test/TestCase1s.ll
+++ Assignment1-Introduction_to_LLVM/LocalOpts/test/TestCase1s.ll
@@ -5,16 +5,15 @@ source_filename = "./test/TestCase1.ll"
 
 define dso_local void @foo(i32 noundef %0) {
   %2 = add nsw i32 %0, 0
-  %3 = shl i32 %2, 4
+  %3 = mul nsw i32 %2, 16
   %4 = mul nsw i32 %3, %2
   %5 = sdiv i32 %4, %0
   %6 = sdiv i32 %4, 10
   %7 = mul nsw i32 54, %5
-  %8 = ashr i32 %6, 7
-  %9 = sdiv i32 %7, 54
-  %10 = sdiv i32 %6, 1
-  %11 = sub nsw i32 %9, 0
-  %12 = call i32 (ptr, ...) @printf(ptr noundef @.str, i32 noundef %2, i32 noundef %3, i32 noundef %4, i32 noundef %5, i32 noundef %6, i32 noundef %7, i32 noundef %8, i32 noundef %9, i32 noundef %10, i32 noundef %11)
+  %8 = sdiv i32 %6, 128
+  %9 = sdiv i32 %6, 1
+  %10 = sub nsw i32 %5, 0
+  %11 = call i32 (ptr, ...) @printf(ptr noundef @.str, i32 noundef %2, i32 noundef %3, i32 noundef %4, i32 noundef %5, i32 noundef %6, i32 noundef %7, i32 noundef %8, i32 noundef %5, i32 noundef %9, i32 noundef %10)
   ret void
 }
 
